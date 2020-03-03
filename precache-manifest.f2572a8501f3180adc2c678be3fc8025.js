24096d8752e9a6ed0d3913615464f887d57576d3
Ewa Potorecka <ewa.potorecka@gmail.com>
2020-03-03 15:47:21 +0100

    Updates

---
+++ precache-manifest.f2572a8501f3180adc2c678be3fc8025.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e2c0a9e043ce1be7379730489d143bb",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "ffb2ef7b4bcb2001e663",
+    "url": "/rock-paper-scissors/static/css/main.c6feac27.chunk.css"
+  },
+  {
+    "revision": "f47ce849f48adf2d6964",
+    "url": "/rock-paper-scissors/static/js/2.23a56dbc.chunk.js"
+  },
+  {
+    "revision": "ffb2ef7b4bcb2001e663",
+    "url": "/rock-paper-scissors/static/js/main.5c749712.chunk.js"
+  },
+  {
+    "revision": "8b71bd09ba6d6295ba53",
+    "url": "/rock-paper-scissors/static/js/runtime-main.a5b782b9.js"
+  },
+  {
+    "revision": "401eaf4352c23c30627aa36caeb617e8",
+    "url": "/rock-paper-scissors/static/media/paper.401eaf43.svg"
+  },
+  {
+    "revision": "eb1bf1895e5292b22b8f06c719f22d31",
+    "url": "/rock-paper-scissors/static/media/rock.eb1bf189.svg"
+  },
+  {
+    "revision": "cb7ff8ea93ec27a94d392e5b126184d8",
+    "url": "/rock-paper-scissors/static/media/scissors.cb7ff8ea.svg"
+  }
+]);
